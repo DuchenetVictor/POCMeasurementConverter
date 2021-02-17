a654c91e565b6d8d12d7770ce0449c71aad3f729
victor.duchenet <victorduchenet@gmail.com>
2021-02-17 21:57:11 +0100

    add validation in measurement field

--- POCMeasurementConverter/View/MainWindow.xaml.cs
+++ POCMeasurementConverter/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿//using System.Windows.Input;
 
 using System.Text.RegularExpressions;
+using System.Windows.Controls;
 using System.Windows.Input;
 using MahApps.Metro.Controls;
 
@@ -15,5 +16,23 @@ namespace POCMeasurementConverter.View
         {
             InitializeComponent();
         }
+
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var ue = e.Source as TextBox;
+            Regex regex;
+            if (ue.Text.Contains(","))
+            {
+                regex = new Regex("[^0-9]+");
+            }
+            else
+            {
+                regex = new Regex("[^0-9,]+");
+            }
+
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
     }
 }
